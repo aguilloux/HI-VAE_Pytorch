60ece122b55c635a1d2ac66998aa4ef073d8e3ed
Van-Tuan-NGUYEN <tuan.nguyen2017@ict.jvn.edu.vn>
2025-03-03 17:56:33 +0100

    Support the Kaplan Meier curve for visualizing survival data

--- utils/visualization.py
+++ utils/visualization.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="whitegrid", font="STIXGeneral", context="talk", palette="colorblind")
 
+from lifelines import KaplanMeierFitter
 
 def plot_data(data, feat_types_dict):
     """
@@ -54,10 +55,12 @@ def plot_data(data, feat_types_dict):
                 ax.set_xticklabels([label.get_text() for label in reduced_labels])  # Set labels
 
         elif feature_type in ["surv"]:
-            feature_data = pd.DataFrame(data[:, feat_idx], columns=[feat_name])
-            sns.histplot(feature_data, kde=False, color="royalblue", ax=ax)
             ax.set_title(f"Distribution plot of {feat_name} ({feature_type})", fontsize=16, fontweight="bold")
-            ax.legend().set_visible(False)
+            kmf = KaplanMeierFitter()
+            survival_time, censoring_indicator = data[:, feat_idx : feat_idx + 2].T
+            kmf.fit(survival_time, censoring_indicator, label="Survival time").plot(ax=ax, c='r')
+            kmf.fit(survival_time, 1 - censoring_indicator, label="Censoring time").plot(ax=ax, c='b')
+            ax.legend().set_visible(True)
             feat_idx += 1
 
         else:
@@ -86,6 +89,10 @@ def plot_true_vs_estimation(true_values, estimated_values, miss_mask, types_dict
     - num_sel_samples (int): Number of random samples to plot (default: 100).
     """
     num_feats = len(types_dict)
+    for feat_type in types_dict:
+        if feat_type["type"] in ['surv']:
+            num_feats += 1
+
     fig, axes = plt.subplots(num_feats, 2, figsize=(20, 4 * num_feats))
 
     if num_feats == 1:
@@ -101,7 +108,6 @@ def plot_true_vs_estimation(true_values, estimated_values, miss_mask, types_dict
             true_missing = true_values[true_values[:, feat_idx + 1] == 0, feat_idx].cpu().numpy()
             est_observed = estimated_values[true_values[:, feat_idx + 1] == 1, feat_idx].cpu().numpy()
             est_missing = estimated_values[true_values[:, feat_idx + 1] == 0, feat_idx + 1].cpu().numpy()
-            feat_idx += 1
         else:
             # Extract observed and missing data
             true_observed = true_values[miss_mask[:, i] == 1, feat_idx].cpu().numpy()
@@ -120,49 +126,91 @@ def plot_true_vs_estimation(true_values, estimated_values, miss_mask, types_dict
         true_missing_subset, est_missing_subset = true_missing[miss_indices], est_missing[miss_indices]
 
         # Plot observed values
-        ax_obs = axes[i, 0]
         if feature_type in ['surv']:
-            ax_obs.set_title(f"Feature {i+1}: {feature_type} (Survival time)", fontsize=15, fontweight='bold')
+            ax_obs = axes[feat_idx : feat_idx + 2, 0]
+            ax_obs[0].set_title(f"Feature {i+1}: {feature_type} (Survival time)", fontsize=15, fontweight='bold')
+            ax_obs[1].set_title(f"Feature {i+1}: {feature_type} (Kaplan–Meier Survival time)", fontsize=15, fontweight='bold')
+            ax_obs[0].set_xlabel("Sample Index")
+            ax_obs[0].set_ylabel("Value")
+            ax_obs[0].grid(True)
+            ax_obs[1].grid(True)
+
         else:
+            ax_obs = axes[feat_idx, 0]
             ax_obs.set_title(f"Feature {i+1}: {feature_type} (Observed)", fontsize=15, fontweight='bold')
-        ax_obs.set_xlabel("Sample Index")
-        ax_obs.set_ylabel("Value")
-        ax_obs.grid(True)
+            ax_obs.set_xlabel("Sample Index")
+            ax_obs.set_ylabel("Value")
+            ax_obs.grid(True)
 
         # Plot missing values
-        ax_miss = axes[i, 1]
         if feature_type in ['surv']:
-            ax_miss.set_title(f"Feature {i+1}: {feature_type} (Censoring time)", fontsize=15, fontweight='bold')
+            ax_miss = axes[feat_idx : feat_idx + 2, 1]
+            ax_miss[0].set_title(f"Feature {i+1}: {feature_type} (Censoring time)", fontsize=15, fontweight='bold')
+            ax_miss[1].set_title(f"Feature {i+1}: {feature_type} (Kaplan–Meier Censoring time)", fontsize=15, fontweight='bold')
+            ax_miss[0].set_xlabel("Sample Index")
+            ax_miss[0].set_ylabel("Value")
+            ax_miss[0].grid(True)
+            ax_miss[1].grid(True)
         else:
+            ax_miss = axes[feat_idx, 1]
             ax_miss.set_title(f"Feature {i+1}: {feature_type} (Missing)", fontsize=15, fontweight='bold')
-        ax_miss.set_xlabel("Sample Index")
-        ax_miss.set_ylabel("Value")
-        ax_miss.grid(True)
+            ax_miss.set_xlabel("Sample Index")
+            ax_miss.set_ylabel("Value")
+            ax_miss.grid(True)
 
-        if feature_type in ['real', 'pos', 'cat', 'surv']:  # Continuous & categorical data
-            ax_obs.scatter(range(num_obs_samples), true_observed_subset, label="True", marker='o', alpha=0.6)
-            ax_obs.scatter(range(num_obs_samples), est_observed_subset, label="Estimation", marker='x', alpha=0.6)
 
-            ax_miss.scatter(range(num_miss_samples), true_missing_subset, label="True", marker='o', alpha=0.6)
-            ax_miss.scatter(range(num_miss_samples), est_missing_subset, label="Estimation", marker='x', alpha=0.6)
+        if feature_type in ['surv']:  # Continuous & categorical data
+            ax_obs[0].scatter(range(num_obs_samples), true_observed_subset, label="True", marker='o', alpha=0.6)
+            ax_obs[0].scatter(range(num_obs_samples), est_observed_subset, label="Estimation", marker='x', alpha=0.6)
+            ax_obs[0].legend()
 
-        elif feature_type == 'count':  # Count data
-            ax_obs.bar(range(num_obs_samples), true_observed_subset, label="True", alpha=0.6)
-            ax_obs.bar(range(num_obs_samples), est_observed_subset, label="Estimation", alpha=0.6)
+            true_survival_time = true_values[:, feat_idx].cpu().numpy()
+            true_censoring_indicator = true_values[:, feat_idx + 1].cpu().numpy()
+            est_survival_time = estimated_values[:, feat_idx].cpu().numpy()
+            est_censoring_indicator = (estimated_values[:, feat_idx] < estimated_values[:, feat_idx + 1]).cpu().numpy()
 
-            ax_miss.bar(range(num_miss_samples), true_missing_subset, label="True", alpha=0.6)
-            ax_miss.bar(range(num_miss_samples), est_missing_subset, label="Estimation", alpha=0.6)
+            kmf = KaplanMeierFitter()
+            kmf.fit(true_survival_time, true_censoring_indicator, label="True Survival time").plot(ax=ax_obs[1], c='r')
+            kmf.fit(est_survival_time, est_censoring_indicator, label="Estimated Survival time").plot(ax=ax_obs[1], c='r', marker='+')
+            ax_obs[1].legend()
 
-        elif feature_type == 'ordinal':  # Ordinal (Thermometer Encoding)
-            ax_obs.plot(range(num_obs_samples), true_observed_subset, label="True", marker='o')
-            ax_obs.plot(range(num_obs_samples), est_observed_subset, label="Estimation", marker='x')
+            ax_miss[0].scatter(range(num_miss_samples), true_missing_subset, label="True", marker='o', alpha=0.6)
+            ax_miss[0].scatter(range(num_miss_samples), est_missing_subset, label="Estimation", marker='x', alpha=0.6)
+            ax_miss[0].legend()
 
-            ax_miss.plot(range(num_miss_samples), true_missing_subset, label="True", marker='o')
-            ax_miss.plot(range(num_miss_samples), est_missing_subset, label="Estimation", marker='x')
+            kmf = KaplanMeierFitter()
+            kmf.fit(true_survival_time, 1 - true_censoring_indicator, label="True Censoring time").plot(ax=ax_miss[1], c='b', )
+            kmf.fit(est_survival_time, 1 - est_censoring_indicator, label="Estimated Censoring time").plot(ax=ax_miss[1], c='b', marker='+')
+            ax_miss[1].legend()
+        else:
+            if feature_type in ['real', 'pos', 'cat']:  # Continuous & categorical data
+                ax_obs.scatter(range(num_obs_samples), true_observed_subset, label="True", marker='o', alpha=0.6)
+                ax_obs.scatter(range(num_obs_samples), est_observed_subset, label="Estimation", marker='x', alpha=0.6)
 
-        ax_obs.legend()
-        ax_miss.legend()
-        feat_idx += 1
+                ax_miss.scatter(range(num_miss_samples), true_missing_subset, label="True", marker='o', alpha=0.6)
+                ax_miss.scatter(range(num_miss_samples), est_missing_subset, label="Estimation", marker='x', alpha=0.6)
+
+            elif feature_type == 'count':  # Count data
+                ax_obs.bar(range(num_obs_samples), true_observed_subset, label="True", alpha=0.6)
+                ax_obs.bar(range(num_obs_samples), est_observed_subset, label="Estimation", alpha=0.6)
+
+                ax_miss.bar(range(num_miss_samples), true_missing_subset, label="True", alpha=0.6)
+                ax_miss.bar(range(num_miss_samples), est_missing_subset, label="Estimation", alpha=0.6)
+
+            elif feature_type == 'ordinal':  # Ordinal (Thermometer Encoding)
+                ax_obs.plot(range(num_obs_samples), true_observed_subset, label="True", marker='o')
+                ax_obs.plot(range(num_obs_samples), est_observed_subset, label="Estimation", marker='x')
+
+                ax_miss.plot(range(num_miss_samples), true_missing_subset, label="True", marker='o')
+                ax_miss.plot(range(num_miss_samples), est_missing_subset, label="Estimation", marker='x')
+
+            ax_obs.legend()
+            ax_miss.legend()
+
+        if feature_type in ['surv']:
+            feat_idx += 2
+        else:
+            feat_idx += 1
 
     plt.suptitle("True vs. Estimated Values for Different Data Types", fontsize=20, fontweight='bold')
     plt.tight_layout(rect=[0, 0., 1., .98])  # Adjust for suptitle
