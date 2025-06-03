aacd3160c26161d3fb20e315f595badb01ad7631
Van-Tuan-NGUYEN <tuan.nguyen2017@ict.jvn.edu.vn>
2025-06-03 14:33:50 +0200

    Minor update

--- simulation_experiment.py
+++ simulation_experiment.py
@@ -28,7 +28,7 @@ def run():
     control, treated, types = simulation(treatment_effect, n_samples, independent = False, surv_type = 'surv_piecewise', 
                                         feature_types_list = ["pos", "real", "cat"], n_features_bytype = 4, 
                                         n_active_features = 3 , p_treated = 0.5, shape_T = 2, shape_C = 2, scale_C = 6., 
-                                        scale_C_indep = 2.5, data_types_create = True, seed=0)
+                                        scale_C_indep = 4.5, data_types_create = True, seed=0)
     control = control.drop(columns='treatment')
     treated = treated.drop(columns='treatment')
 
